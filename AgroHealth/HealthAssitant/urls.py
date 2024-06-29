21b16f4693f9f941e87882e1138aff71438405a9
Joshua J Smith <jliansyy2001@gmail.com>
2024-06-29 05:31:51 +0200

    Update urls.py

--- AgroHealth/HealthAssitant/urls.py
+++ AgroHealth/HealthAssitant/urls.py
@@ -5,10 +5,8 @@ from . import views
 urlpatterns = [
     path('upload/crop/', views.CropImageUpload.as_view(), name='upload_crop_image'),
     path('upload/animal/', views.AnimalImageUpload.as_view(), name='upload_animal_image'),
-
-path('detect/disease/animal', views.DiseaseDetection.as_view(), name='detect_disease'),
+    path('detect/disease/animal/', views.DiseaseDetectionAndTreatment.as_view(), name='detect_animal_disease'),
+    path('detect/disease/crop/', views.CropDiseaseDetection.as_view(), name='detect_crop_disease'),
     path('recommend/treatment/<str:endpoint>/', views.TreatmentRecommendation.as_view(), name='recommend_treatment'),
 ]
-
-
 
